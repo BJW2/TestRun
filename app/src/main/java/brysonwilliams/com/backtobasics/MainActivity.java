1d3844da10f008ff01be23c57c685eac615b97bf
Bryson Williams <carabrysonw@gmail.com>
2021-10-14 09:25:33 +1000

    14/10/2021

--- app/src/main/java/brysonwilliams/com/backtobasics/MainActivity.java
+++ app/src/main/java/brysonwilliams/com/backtobasics/MainActivity.java
@@ -3,18 +3,28 @@ package brysonwilliams.com.backtobasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    //FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    MediaPlayer mp;
+    MediaPlayer mp1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mp = MediaPlayer.create(MainActivity.this, R.raw.rap);
+        mp1 = MediaPlayer.create(MainActivity.this,R.raw.gunshot);
     }
 
     public void toastMsg(String msg) { // creates a pop up message
@@ -24,14 +34,32 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View view) {
 
-        EditText editPassword = findViewById(R.id.editTextPassword);
-        TextView textPassword = findViewById(R.id.textViewPassword);
-        String content = editPassword.getText().toString(); //gets you the content of edit text
+        mp.start();
 
-        if (content.equals("Password")) {
-            startActivity(new Intent(MainActivity.this, MainActivity2.class)); // Navigate to another activity
-        } else {
-            toastMsg("Wrong PW Bruv, You are not good at this mate, just stop!!!");
-        }
+        // EditText editPassword = findViewById(R.id.editTextPassword);
+        //TextView textPassword = findViewById(R.id.textViewPassword);
+        //String content = editPassword.getText().toString(); //gets you the content of edit text
+
+        //if (content.equals("Password")) {
+        //startActivity(new Intent(MainActivity.this, MainActivity2.class)); // Navigate to another activity
+        //} else {
+        //  toastMsg("Wrong PW Bruv, You are not good at this mate, just stop!!!");
+    }
+
+
+
+    public void onClick2(View view) {
+        //DatabaseReference myRef = database.getReference("message");
+
+        //myRef.setValue("Goodbye, World!")
+        mp1.start();
+    }
+
+    public void onClick3(View view) {
+        //DatabaseReference myRef = database.getReference("message");
+
+        //myRef.setValue("Goodbye, World!")
+        mp.stop();
+        mp1.stop();
     }
 }
